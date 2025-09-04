7faee9c90c379976561fd2f4660ff5420be267de
Jhoni Cunha <jhonicunha@gmail.com>
2025-09-04 19:29:50 -0300

    Desafio nível Aventureiro concluído

--- batalha_naval.c
+++ batalha_naval.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-//Desafio Jogo de Batalha Naval - Nível Novato
+// Desafio Jogo de Batalha Naval - Nível Aventureiro
+
 
 int main() {
 
-    // Criando o tabuleiro e inicializando com água (0)
+    // Criando o Tabuleiro
     int tabuleiro[10][10];
     int linha, coluna;
 
@@ -14,21 +15,31 @@ int main() {
         }
     }
 
-    // Posicionando os Navios (numero 3)
+    // Posicionar os Quatro Navios (3)
     int i;
-    
-    // Posicionando Navio 1
+
+    // Navio 1 (Horizontal) na linha 2, colunas 2, 3 e 4
     for (i = 0; i < 3; i++) {
         tabuleiro[2][2 + i] = 3;
     }
-    
-    // Posicionando Navio 2
+
+    // Navio 2 (Vertical) na coluna 5, linhas 5, 6 e 7
     for (i = 0; i < 3; i++) {
         tabuleiro[5 + i][5] = 3;
     }
 
-    // Imprimir o Tabuleiro
-    printf("Tabuleiro Batalha Naval\n");
+    // Navio 3 (Diagonal Principal) nas posições [0][0], [1][1], [2][2]
+    for (i = 0; i < 3; i++) {
+        tabuleiro[0 + i][0 + i] = 3;
+    }
+
+    // Navio 4 (Diagonal Secundária) nas posições [0][9], [1][8], [2][7]
+    for (i = 0; i < 3; i++) {
+        tabuleiro[0 + i][9 - i] = 3;
+    }
+
+    // Imprimir o Tabuleiro Final
+    printf("Tabuleiro Batalha Naval (Nivel Aventureiro)\n");
     for (linha = 0; linha < 10; linha++) {
         for (coluna = 0; coluna < 10; coluna++) {
             printf("%d ", tabuleiro[linha][coluna]);
